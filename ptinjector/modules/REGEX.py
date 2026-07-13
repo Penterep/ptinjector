477c6f98bc58325b8788c4d12eafe481227fa6fd
Daniel Kümmel <d.kummel@penterep.com>
2026-07-13 14:43:51 +0200

    fix: compare regex findings against baseline response

--- ptinjector/modules/REGEX.py
+++ ptinjector/modules/REGEX.py
@@ -1,17 +1,25 @@
 import re
 
 def run(payload_object, definition_contents, request_data, injector):
-    responses = []
-    payloads = []
+    baseline_response, _ = injector.run_payload_str(request_data, injector.RANDOM_STRING)
+
     for payload_str in payload_object["payload"]:
         response, dump = injector.run_payload_str(request_data, payload_str)
-        yield [payload_str], [response], dump
+        yield [payload_str], [baseline_response, response], dump
 
 
 def check_if_vulnerable(responses, verification_list, injector):
-    """Check if <verification_re> in <response.text>"""
-    response = responses.pop()
-    """Check if <verification_re> in <response.text>"""
-    verification_re = '(' + ')|('.join(verification_list) + ')'
-    verification_re = re.compile(verification_re)
-    return bool(re.search(verification_re, response.text))
+    """Return True when a verification pattern appears only after injection."""
+    if len(responses) != 2 or not verification_list:
+        return False
+
+    try:
+        verification_re = re.compile('(' + ')|('.join(verification_list) + ')')
+    except (TypeError, re.error):
+        return False
+
+    baseline_response, payload_response = responses
+    baseline_matches = re.search(verification_re, baseline_response.text)
+    payload_matches = re.search(verification_re, payload_response.text)
+
+    return payload_matches is not None and baseline_matches is None
