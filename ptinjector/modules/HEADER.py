608b43f45dfd839176560a350a6a651378f5a7b3
Daniel Kümmel <d.kummel@penterep.com>
2026-07-13 14:45:30 +0200

    fix: require newly injected response headers

--- ptinjector/modules/HEADER.py
+++ ptinjector/modules/HEADER.py
@@ -1,14 +1,20 @@
 
 
 def run(payload_object, definition_contents, request_data, injector):
-    responses = []
-    payloads = []
+    baseline_response, _ = injector.run_payload_str(request_data, injector.RANDOM_STRING)
+
     for payload_str in payload_object["payload"]:
         response, dump = injector.run_payload_str(request_data, payload_str)
-        yield [payload_str], [response], dump
+        yield [payload_str], [baseline_response, response], dump
 
 
 def check_if_vulnerable(responses, verification_list, injector):
-    response = responses[0]
-    return True if any([any(verification_string in header_name for verification_string in verification_list) for header_name in response.headers.keys()]) else False
+    """Return True when injection adds an expected response header."""
+    if len(responses) != 2 or not verification_list:
+        return False
 
+    expected_headers = {header.casefold() for header in verification_list}
+    baseline_headers = {header.casefold() for header in responses[0].headers}
+    payload_headers = {header.casefold() for header in responses[1].headers}
+
+    return bool((payload_headers - baseline_headers) & expected_headers)
